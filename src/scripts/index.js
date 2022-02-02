c36682b968af47582dafa3ec71d51bbbe812a493
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-02-02 11:58:15 -0500

    Add functions to count the list of items and display them in the browser

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,11 @@
 // Include your code here
 import './styles/main.scss';
-import { displayPokemons, displayLikes } from './modules/generators.js';
+import { displayPokemons, displayLikes, displayCounters } from './modules/generators.js';
 import displayPopup from './modules/popup.js';
 
 window.onload = async () => {
   await displayPokemons();
   await displayLikes();
   displayPopup();
+  displayCounters();
 };
